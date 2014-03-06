b13970790f262ffca6510db2c8b6c72a54c48002
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-06 12:39:03 +0000

    updated app to use config from lazyconf

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
+import os
+import lazyconf
 from flask import Flask
+
 app = Flask(__name__)
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+lazy = lazyconf.Lazyconf().load(base_dir)
 
 @app.route('/')
 def hello_world():
     return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host=lazy.get('host'))
